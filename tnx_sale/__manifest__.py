ff18be4e10cf30ed29bf6ddb81a189d2657f6d6e
ranoa <tsiky@alphamada.com>
2022-03-31 10:04:40 +0300

    FEAT[tnx_sale]:add field spec in sale order

--- tnx_sale/__manifest__.py
+++ tnx_sale/__manifest__.py
@@ -20,13 +20,14 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','tnx_base'],
+    'depends': ['base','tnx_base','sale'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/sale_order.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
---
+++ tnx_sale/models/sale_order.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Sale_order(models.Model):
+    _inherit="sale.order"
+    sale_order_partner = fields.Char('BC client')
+    production_duration = fields.Char('Durée de fabrication')
+    delivery_time = fields.Char('Délais de livraison')
