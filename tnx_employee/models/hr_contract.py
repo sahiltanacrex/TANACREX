f17f26b9bb1a41277ab5c1a23cbcd429dfc91876
ranoa <tsiky@alphamada.com>
2022-04-27 11:59:17 +0300

    FEAT[tnx_employee]:add compute to wage and get base.salary

--- tnx_employee/models/hr_contract.py
+++ tnx_employee/models/hr_contract.py
@@ -17,6 +17,11 @@ class HrContractInherit(models.Model):
     production_bonus_participant = fields.Monetary('Prime de production participant')
     allow_transport = fields.Monetary('Indemnité de transport')
     allow_family = fields.Monetary('Allocations familiales')
+    wage = fields.Monetary('Wage', required=True, tracking=True, help="Employee's monthly gross wage.", compute='_compute_base_salary' )
+    
+    def _compute_base_salary(self):
+        for record in self:
+            record.wage = record.base_salary
 
     @api.depends('hour_per_week')
     def _get_hourly_salary(self):
