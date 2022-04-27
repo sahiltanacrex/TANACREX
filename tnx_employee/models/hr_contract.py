6f6f67f914514cf86663f5a2c08786674382f0cd
AlphaConseils <alpha@alphamada.com>
2022-04-27 19:00:15 +0200

    FIX[tnx_payroll] : review report and hr.contract.history.view

--- tnx_employee/models/hr_contract.py
+++ tnx_employee/models/hr_contract.py
@@ -6,7 +6,9 @@ class HrContractInherit(models.Model):
     contrat_categorie = fields.Selection(
         [('cdi', 'CDI'),
          ('cdd', 'CDD'),
-         ('stagiaire', 'Stagiaire')], string='Categorie de contrat', default="cdi",
+         ('stagiaire', 'Stagiaire'),
+         ('temporaire', 'Temporaire')
+         ], string='Categorie de contrat', default="cdi",
         required=True, )
     hour_per_week = fields.Float(
         string="Nombre d'heures travaillé par semaine", related="resource_calendar_id.full_time_required_hours",
@@ -17,8 +19,9 @@ class HrContractInherit(models.Model):
     production_bonus_participant = fields.Monetary('Prime de production participant')
     allow_transport = fields.Monetary('Indemnité de transport')
     allow_family = fields.Monetary('Allocations familiales')
-    wage = fields.Monetary('Wage', required=True, tracking=True, help="Employee's monthly gross wage.", compute='_compute_base_salary' )
-    
+    wage = fields.Monetary('Wage', required=True, tracking=True, help="Employee's monthly gross wage.",
+                           compute='_compute_base_salary')
+
     def _compute_base_salary(self):
         for record in self:
             record.wage = record.base_salary
