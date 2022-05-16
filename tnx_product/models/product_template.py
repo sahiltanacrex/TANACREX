685dc76c037c38496b3403a91240162db89832d8
ranoa <tsiky@alphamada.com>
2022-05-16 16:26:06 +0300

    FEAT[tnx_product]:[Article reference]Add fields

--- tnx_product/models/product_template.py
+++ tnx_product/models/product_template.py
@@ -7,41 +7,31 @@ class Product_template(models.Model):
         string='HS CODE',
         required=True)
 
-    product_format = fields.Selection([
-        ('circle', 'Cercle'),
-        ('rectangle', 'Rectangle'),
-        ('other', 'Autres'),
-    ], string='Format du produit')
-
-    surface = fields.Float('Surface' )
-
-    @api.onchange('product_format')
-    def _onchange_product_format(self):
-        for record in self:
-            record.length=0
-            record.height=0
-            record.diameter=0
+    # product_format = fields.Selection([
+    #     ('circle', 'Cercle'),
+    #     ('rectangle', 'Rectangle'),
+    #     ('other', 'Autres'),
+    # ], string='Format du produit')
     
-    @api.onchange('diameter')
-    def _change_surface_circle(self):
-        for record in self:
-            record.surface= pi * record.diameter     
+    
 
-    @api.onchange('height','length')
-    def _change_surface_rectangle(self):
+    @api.onchange('product_type')
+    def _onchange_surface(self):
         for record in self:
-            record.surface= record.length * record.height
+            if record.product_type == 'button':
+                record.surface= record.line * record.diameter   
+                record.length=0
+                record.height=0
+                record.surface=0
+            elif record.product_type in ['label','sticker']:
+                record.surface = record.length * record.height
+                record.line=0
+                record.diameter=0
+                record.surface=0
+                record.number_holes=0
+            # elif record.product_type == 'other':
+            #     record.surface = 0
             
-     
-    length = fields.Float(
-        string='Longueur mm',
-        required=False)
-    height = fields.Float(
-        string='Hauteur mm',
-        required=False)
-    diameter = fields.Float(
-        string='Diamètre mm',
-        required=False)
     
     origin = fields.Char(
         string='Origine', 
@@ -62,7 +52,40 @@ class Product_template(models.Model):
     miscellaneous = fields.Char(
         string='Divers', 
         required=False)
-    
+
+    #! add fields
+    product_type = fields.Selection([
+        ('button', 'Boutton'),
+        ('label', 'Etiquette'),
+        ('sticker', 'Autocollant'),
+        ('other', 'Autres'),
+    ], string='Type du produit')
+
+    material = fields.Char('Matière')
+    thickness = fields.Char('Epaisseur')
+    customer_reference = fields.Char('Référence Client')
+    partner_id = fields.Many2one('res.partner', string='Client')
+
+    # field based on condition
+    #?if button_type = button
+    line = fields.Float('Ligne')
+    diameter = fields.Float(
+        string='Diamètre mm',
+        required=False)
+
+    surface = fields.Float('Surface')
+
+    number_holes = fields.Integer('Nombre de trous')
+
+    #? if sticker or a label
+    #! search surface  by this fields
+    length = fields.Float(
+        string='Longueur mm',
+        required=False)
+    height = fields.Float(
+        string='Hauteur mm',
+        required=False)
+
 
     # TODO BAT here
 
