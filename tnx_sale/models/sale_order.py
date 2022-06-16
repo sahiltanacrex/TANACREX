741e619ef280b59aead584d88a233c8a81b41f33
rivoalpha <rivo@alphamada.com>
2022-06-16 11:15:55 +0300

    FEAT [tnx_sale] ADD fields prix au forfait on product [RV]

--- tnx_sale/models/sale_order.py
+++ tnx_sale/models/sale_order.py
@@ -2,11 +2,9 @@
 
 from odoo import models, fields, api
 
-class Sale_order(models.Model):
-    _inherit="sale.order"
-    sale_order_partner = fields.Char('BC client')
-    production_duration = fields.Char('Durée de fabrication',required=True)
-    delivery_time = fields.Char('Délais de livraison',required=True)
-    print("GG")
 
-    
+class Sale_order(models.Model):
+    _inherit = "sale.order"
+    sale_order_partner = fields.Char("BC client")
+    production_duration = fields.Char("Durée de fabrication", required=True)
+    delivery_time = fields.Char("Délais de livraison", required=True)
--- tnx_sale/models/sale_order_line.py
+++ tnx_sale/models/sale_order_line.py
@@ -4,18 +4,12 @@ from odoo import models, api, fields
 
 
 class Sale_order_line(models.Model):
-    _inherit = 'sale.order.line'
-    development_expenses = fields.Monetary('Frais de développement')
-    unit_qty = fields.Float('Quantité unitaire')
-    product_uom_qty = fields.Float('Quantité cond.')
+    _inherit = "sale.order.line"
+    development_expenses = fields.Monetary("Frais de développement")
+    unit_qty = fields.Float("Quantité unitaire")
+    product_uom_qty = fields.Float("Quantité cond.")
 
-    @api.onchange('unit_qty')
-    def _onchange_unit_qty(self):
-        if self.unit_qty:
-            if self.unit_qty > 0:
-                self.product_uom_qty = self.unit_qty / self.product_uom.ratio
-
-    @api.depends('discount', 'price_unit', 'tax_id', 'development_expenses', 'unit_qty')
+    @api.depends("discount", "price_unit", "tax_id", "development_expenses", "unit_qty")
     def _compute_amount(self):
         """
         tsy override tsony fa tonga dia notsindrina tanteraka sinon tsy miainga
@@ -29,38 +23,55 @@ class Sale_order_line(models.Model):
             for line in rec:
                 price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
                 if line.product_id.qty_min:
-                    if line.unit_qty < line.product_id.qty_min:
-                        price = line.product_id.minimum_price
+                    if line.product_id.qty_min > 0:
+                        if line.unit_qty < line.product_id.qty_min:
+                            price = line.product_id.minimum_price
                 # new: substract development_expenses
-                taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty, product=line.product_id, partner=line.order_id.partner_shipping_id)
-                line.update({
-                    'price_tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
-                    'price_total': taxes['total_included'],
-                    'price_subtotal': taxes['total_excluded'] + line.development_expenses,
-                })
-                if rec.env.context.get('import_file', False) and not rec.env.user.user_has_groups('account.group_account_manager'):
-                    line.tax_id.invalidate_cache(['invoice_repartition_line_ids'], [line.tax_id.id])
+                taxes = line.tax_id.compute_all(
+                    price,
+                    line.order_id.currency_id,
+                    line.product_uom_qty,
+                    product=line.product_id,
+                    partner=line.order_id.partner_shipping_id,
+                )
+                line.update(
+                    {
+                        "price_tax": sum(
+                            t.get("amount", 0.0) for t in taxes.get("taxes", [])
+                        ),
+                        "price_total": taxes["total_included"],
+                        "price_subtotal": taxes["total_excluded"]
+                        + line.development_expenses,
+                    }
+                )
+                if rec.env.context.get(
+                    "import_file", False
+                ) and not rec.env.user.user_has_groups("account.group_account_manager"):
+                    line.tax_id.invalidate_cache(
+                        ["invoice_repartition_line_ids"], [line.tax_id.id]
+                    )
             if rec.unit_qty:
                 if rec.unit_qty > 0:
                     rec.product_uom_qty = rec.unit_qty / rec.product_uom.ratio
             if rec.product_id.qty_min:
-                if rec.unit_qty < rec.product_id.qty_min:
-                    self.price_subtotal = self.product_id.minimum_price
+                if rec.product_id.qty_min > 0:
+                    if rec.unit_qty < rec.product_id.qty_min:
+                        rec.price_subtotal = rec.product_id.minimum_price
 
     def _prepare_invoice_line(self, **optional_values):
         """
-            add order_id.name
+        add order_id.name
         """
         values = super(Sale_order_line, self)._prepare_invoice_line(**optional_values)
 
-        values['development_expenses'] = self.development_expenses
-        values['price_subtotal'] = self.development_expenses + self.price_subtotal
-        values['unit_qty'] = self.unit_qty
+        values["development_expenses"] = self.development_expenses
+        values["price_subtotal"] = self.development_expenses + self.price_subtotal
+        values["unit_qty"] = self.unit_qty
         # values["move_line_ids"] = [(4, m.id) for m in stock_moves]
 
         return values
 
-    @api.onchange('price_unit', 'product_id')
+    @api.onchange("unit_qty")
     def _onchange_price_unit(self):
         """test price units
 
@@ -69,39 +80,40 @@ class Sale_order_line(models.Model):
             raise a pop up and put price_unit at zero
 
         """
+        if self.unit_qty:
+            if self.unit_qty > 0:
+                self.product_uom_qty = self.unit_qty / self.product_uom.ratio
+
         if self.product_id.qty_min:
+            if self.product_id.qty_min > 0:
+                if self.unit_qty < self.product_id.qty_min:
+                    self.price_subtotal = self.product_id.minimum_price
+                    # !this return pop up
+                    return {
+                        "warning": {
+                            "title": "Quantité minimum Non atteint!",
+                            "message": f"Attention ! La quantité minimum pour ce produit {self.product_id.name} a pas été atteint. Merci de prévoir un forfait.",
+                        }
+                    }
 
-            if self.unit_qty < self.product_id.qty_min:
-                self.price_subtotal = self.product_id.minimum_price
-                # !this return pop up
-                return {
-                    'warning': {
-                        'title': 'Quantité minimum Non atteint!',
-                        'message': f'Attention ! La quantité minimum pour ce produit {self.product_id.name} a pas été atteint. Merci de prévoir un forfait.'}
-
-                }
-
-    @api.onchange('product_uom_qty')
+    @api.onchange("product_uom_qty")
     def _onchangeqty_min_product_uom_qty(self):
         self.unit_qty = self.product_uom_qty * self.product_uom.ratio
-        if self.product_id.qty_min and self.product_id and self.product_uom_qty and self.product_id.detailed_type == 'product':
-            if self.unit_qty < self.product_id.qty_min:
-                self.price_subtotal = self.product_id.minimum_price
-                return {
-
-                    'warning': {
-
-                        'title': 'Quantité minimum Non atteint!',
-
-                        'message': f'Attention ! La quantité minimum pour ce produit {self.product_id.name} n\'a pas été atteint. Merci de prévoir un forfait.'}
-
-                }
+        if (
+            self.product_id.qty_min
+            and self.product_id
+            and self.product_uom_qty
+            and self.product_id.detailed_type == "product"
+        ):
+            if self.product_id.qty_min > 0:
+                if self.unit_qty < self.product_id.qty_min:
+                    self.price_subtotal = self.product_id.minimum_price
 
 
 class Account_move_line(models.Model):
-    _inherit = 'account.move.line'
-    development_expenses = fields.Monetary('Frais de développement')
-    unit_qty = fields.Float('Quantité unitaire')
+    _inherit = "account.move.line"
+    development_expenses = fields.Monetary("Frais de développement")
+    unit_qty = fields.Float("Quantité unitaire")
 
     # price_subtotal = fields.Monetary(compute='_compute_price_subtotal')
     # @api.depends('development_expenses')
@@ -112,10 +124,20 @@ class Account_move_line(models.Model):
     #             val.price_subtotal += val.development_expenses
 
     @api.model
-    def _get_price_total_and_subtotal_model(self, price_unit, quantity, discount, currency, product, partner, taxes, move_type):
-        values = super(Account_move_line, self)._get_price_total_and_subtotal_model(price_unit, quantity, discount, currency, product, partner, taxes, move_type)
-        values['price_subtotal'] += self.development_expenses
+    def _get_price_total_and_subtotal_model(
+        self,
+        price_unit,
+        quantity,
+        discount,
+        currency,
+        product,
+        partner,
+        taxes,
+        move_type,
+    ):
+        values = super(Account_move_line, self)._get_price_total_and_subtotal_model(
+            price_unit, quantity, discount, currency, product, partner, taxes, move_type
+        )
+        values["price_subtotal"] += self.development_expenses
 
         return values
-    # def _get_price_total_and_subtotal_model(self, price_unit, quantity,
-    # discount ,currency, product, partner, taxes, move_type)
