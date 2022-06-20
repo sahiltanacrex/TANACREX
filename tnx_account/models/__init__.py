9a47eb81e7c9994785b77d8dab82dddedfb48ac7
rivoalpha <rivo@alphamada.com>
2022-06-21 01:37:26 +0300

    FEAT [tnx_account,tnx_stock] Manage package articles [RV]

--- tnx_account/models/__init__.py
+++ tnx_account/models/__init__.py
@@ -8,5 +8,5 @@ from . import stock_move
 from . import stock_picking
 from . import sequences
 from . import bank_company
-
+from . import stock_move_line
 from . import sale_order
--- tnx_stock/__manifest__.py
+++ tnx_stock/__manifest__.py
@@ -1,38 +1,39 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "tnx_stock",
-
-    'summary': """
+    "name": "tnx_stock",
+    "summary": """
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
+    "description": """
         Long description of module's purpose
     """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
+    "author": "My Company",
+    "website": "http://www.yourcompany.com",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
+    "category": "Uncategorized",
+    "version": "0.1",
     # any module necessary for this one to work correctly
-    'depends': ['base','tnx_base','stock','product',],
-
+    "depends": [
+        "base",
+        "tnx_base",
+        "stock",
+        "product",
+    ],
     # always loaded
-    'data': [
+    "data": [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-        'report/hide_report_picking.xml',
-        'report/report_deliveryslip.xml',
-        'report/product_product_templates.xml',
+        "views/views.xml",
+        "views/templates.xml",
+        "report/hide_report_picking.xml",
+        "report/report_deliveryslip.xml",
+        "report/product_product_templates.xml",
+        "report/note_de_colisage.xml",
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],'license': 'LGPL-3',
+    "demo": [
+        "demo/demo.xml",
+    ],
+    "license": "LGPL-3",
 }
