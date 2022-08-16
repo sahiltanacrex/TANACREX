3a0da200ff6cdbb0726470c021c0676c305cfe39
rivo2302 <rivo2302@gmail.com>
2022-08-16 11:31:48 +0300

    [FIX] tnx_stock,tnx_production : String on fieldds view

--- tnx_stock/models/stock_move_line.py
+++ tnx_stock/models/stock_move_line.py
@@ -6,19 +6,7 @@ from math import ceil
 class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
 
-    @api.onchange("picking_id.move_line_ids_without_package.carton_id")
-    def _get_carton_id_domain(self):
-        # res = []
-        # print("demo")
-        # if self.picking_id:
-        #     for rec in self.picking_id.move_line_ids_without_package:
-        #         if rec.carton_id:
-        #             res.append(rec.carton_id.id)
-        # res = list(set(res))
-        return [("id", "in", [])]
-
     carton_id = fields.Many2one(
         string="Carton",
         comodel_name="stock.carton",
-        domain=lambda self: self._get_carton_id_domain(),
     )
