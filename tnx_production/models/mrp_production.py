dfbe40c8455792b0be0791abfc5b37394b94e7f3
rivo2302 <rivo2302@gmail.com>
2022-10-20 09:56:02 +0300

    [FIX] tnx_production : set reaonly fields to related fields

--- tnx_production/models/mrp_production.py
+++ tnx_production/models/mrp_production.py
@@ -8,28 +8,25 @@ class MrpProduction(models.Model):
 
     _inherit = "mrp.production"
 
-    partner_id = fields.Many2one(
-        "res.partner", string="client", store=True, compute="_compute_source_id"
-    )
-    order_id = fields.Many2one(
-        "sale.order", string="BC", store=True, compute="_compute_source_id"
-    )
+    partner_id = fields.Many2one("res.partner", string="client")
+    order_id = fields.Many2one("sale.order", string="BC")
     developments = fields.Selection(
         [("1", "0%"), ("2", "25%"), ("3", "50%"), ("4", "75%"), ("5", "100%")],
         default="1",
         string="Avancement",
     )
     check_bc = fields.Boolean("Check Bc")
-    image_1920 = fields.Image("Image", compute="_compute_product_id")
+    image_1920 = fields.Image("Image", related="product_id.image_1920")
     bc_client = fields.Char("Bc Client")
-    line_product = fields.Float("Ligne")
-    product_type = fields.Char("Type de produit")
-    color_product = fields.Char("Couleur")
+    line_product = fields.Float("Ligne", related="product_id.line")
+    product_type = fields.Selection(
+        "Type de produit", related="product_id.product_type"
+    )
+    color_product = fields.Char("Couleur", related="product_id.product_tmpl_id.color")
     categorie_id = fields.Many2one(
         "product.category",
         "Catégorie d'article",
         related="product_id.categ_id",
-        store=True,
     )
     date_communication = fields.Date("Date communiquée")
 
@@ -65,11 +62,23 @@ class MrpProduction(models.Model):
                 "context": ctx,
             }
 
-    @api.depends("product_id")
-    def _compute_product_id(self):
-        for val in self:
-            if val.product_id:
-                val.image_1920 = val.product_id.image_1920
+    # @api.depends("origin")
+    # def _compute_source_id(self):
+    #     for production in self:
+    #         if production.origin:
+    #             devis_name = production.origin.split(" - ")
+    #             if len(devis_name) > 1:
+    #                 source = self.env["sale.order"].search(
+    #                     [("name", "=", devis_name[1])]
+    #                 )
+    #                 if source:
+    #                     try:
+    #                         production.order_id = source
+    #                         production.partner_id = source.partner_id
+    #                         production.check_bc = True
+    #                         production.bc_client = source.sale_order_partner
+    #                     except Exception as e:
+    #                         raise UserError(e)
 
     @api.depends("origin")
     def _compute_source_id(self):
@@ -82,17 +91,12 @@ class MrpProduction(models.Model):
                     )
                     if source:
                         try:
-                            production.order_id = source
                             production.partner_id = source.partner_id
                             production.check_bc = True
-                            production.bc_client = source.sale_order_partner
-                            production.line_product = production.product_id.line
-                            production.product_type = production.product_id.product_type
-                            production.color_product = (
-                                production.product_id.product_tmpl_id.color
-                            )
+
                         except Exception as e:
-                            raise UserError(e)
+                            print(e)
+                            pass
 
     def report_analyse(self):
         ids = self.env.context.get("active_ids", [])
