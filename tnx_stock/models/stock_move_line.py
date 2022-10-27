b940f58829a3675c89e88f2602737c8a644e8da9
AlphaConseils <alpha@alphamada.com>
2022-10-27 09:48:09 +0300

    FEAT[tnx_stock]: unit_reserv and unit_done

--- tnx_stock/models/stock_move_line.py
+++ tnx_stock/models/stock_move_line.py
@@ -10,3 +10,28 @@ class StockMoveLine(models.Model):
         string="Carton",
         comodel_name="stock.carton",
     )
+    unit_reserv = fields.Float(
+        "Réservé unitaire",
+        compute="_compute_unit_reserv",
+        store=False,
+    )
+
+    @api.depends("product_uom_qty", "product_uom_id")
+    def _compute_unit_reserv(self):
+        for rec in self:
+            rec.unit_reserv = rec.product_uom_qty * rec.product_uom_id.ratio
+
+    unit_done = fields.Float(
+        "Fait unitaire", default=0.0, digits='Product Unit of Measure', index=1
+    )
+
+    # qty_done = fields.Float('Done', default=0.0, digits='Product Unit of Measure',
+    #                         compute="_compute_unit_done")
+
+    @api.onchange("unit_done")
+    def _compute_unit_done(self):
+        for rec in self:
+            if rec.product_uom_id.ratio == 0:
+                rec.qty_done = rec.unit_done
+            else:
+                rec.qty_done = rec.unit_done / rec.product_uom_id.ratio
