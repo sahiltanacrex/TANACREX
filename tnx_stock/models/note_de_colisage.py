22a23000466dc8947528a2d107aaab8e95b455f9
Warren <nexources@nexources.com>
2022-12-14 10:48:47 +0300

    [FEAT] tnx_stock: packing list

--- tnx_stock/models/note_de_colisage.py
+++ tnx_stock/models/note_de_colisage.py
@@ -1,5 +1,6 @@
 from odoo import _, fields, models, api
 
+from collections import defaultdict
 
 class NoteDeColisage(models.TransientModel):
     _name = "note.colisage.wizard"
@@ -11,17 +12,33 @@ class NoteDeColisage(models.TransientModel):
     poids_net = fields.Float()
     poids_brut = fields.Float()
     description = fields.Char()
-    move_id = fields.Many2one("account.move", string="Facture")
-    origin = fields.Char()
-    inconterm = fields.Char()
-
+    move_id = fields.Many2one("account.move", string="Move")
+    product_ids = fields.Many2many(comodel_name='product.product')
+    inconterm = fields.Text()
+    origin = fields.Text()
+    note_de_colisage_line_ids = fields.One2many(comodel_name='note.de.colisage.line', inverse_name='note_colisage_id')
+    
     @api.onchange('partner_id')
     def onchange_partner_id(self):
-        self.name = 'Picking list' if self.partner_id.lang != 'fr_FR' else 'Note de colisage'
+        self.name = 'Packing list' if self.partner_id.lang != 'fr_FR' else 'Note de colisage'
 
     def report_colisage(self):
         return self.env.ref("tnx_stock.action_tnx_package_report").report_action(self)
-    
+
+    def get_clean_data(self):
+        packings = set(self.note_de_colisage_line_ids.mapped('packing_number'))
+        product_group_by_packings = defaultdict(dict)
+        for packing in packings:
+            packing = int(packing)
+            line_ids = self.note_de_colisage_line_ids.filtered(lambda l: int(l.packing_number) == packing)
+            product_group_by_packings[str(packing)].update({
+                'line_ids': line_ids,
+                'gross_weight': sum(line.gross_weight for line in line_ids),
+                'net_weight': sum(line.net_weight for line in line_ids),
+                'items_count': sum(line.items_count for line in line_ids)
+                })
+        return product_group_by_packings
+
     def get_formatted_qty(self, qty):
         result = f'{qty:,.2f}'.split('.')[0]
         return result.replace(',', ' ')
@@ -29,3 +46,15 @@ class NoteDeColisage(models.TransientModel):
     def get_formatted_weight(self, weight):
         result = f'{weight:,.1f}'
         return result.replace(',', ' ').replace('.', ',')
+
+class NoteDeColisageLine(models.TransientModel):
+    _name = 'note.de.colisage.line'
+
+    product_id = fields.Many2one(comodel_name='product.product')
+    bags = fields.Integer()
+    packing_number = fields.Integer()
+    note_colisage_id  = fields.Many2one(comodel_name='note.colisage.wizard')
+    product_ids = fields.Many2many(comodel_name='product.product', related='note_colisage_id.product_ids')
+    gross_weight = fields.Float()
+    net_weight = fields.Float()
+    items_count = fields.Float()
--- tnx_stock/models/stock_picking.py
+++ tnx_stock/models/stock_picking.py
@@ -1,4 +1,4 @@
-from odoo import _, fields, models, api
+from odoo import _, fields, models
 
 
 class StockPickingInherit(models.Model):
@@ -11,7 +11,8 @@ class StockPickingInherit(models.Model):
         view = self.env.ref("tnx_stock.note_colisage_form_view")
         selected = self.env.context.get("active_ids", [])
         wiz = self.env["note.colisage.wizard"].create(
-            {"name": "Note de colisage", "stock_picking_ids": [(6, 0, selected)]}
+            {"name": "Note de colisage", "stock_picking_ids": [(6, 0, selected)], "product_ids": [
+                (6, 0, self.move_line_ids_without_package.mapped('product_id').ids)]}
         )
         return {
             "name": _("Impréssion note de colisage"),
@@ -27,6 +28,4 @@ class StockPickingInherit(models.Model):
         }
 
     def action_open_label_custom(self):
-        return self.env.ref("tnx_stock.action_etiquette_nexource_report").report_action(
-            self,
-        )
+        return self.env.ref("tnx_stock.action_etiquette_nexource_report").report_action(self)
