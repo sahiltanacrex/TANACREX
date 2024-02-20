36e050c5396b0b51a885b67045bf917c025d9e85
michaelnexources <michael@nexources.com>
2024-02-20 10:16:13 +0300

    #correct fact fusion sale

--- tnx_account/models/sale_order.py
+++ tnx_account/models/sale_order.py
@@ -7,17 +7,18 @@ class Sale_order(models.Model):
     _inherit = "sale.order"
 
     def _create_invoices(self, grouped=False, final=False, date=None):
-        get_currency = self.pricelist_id.currency_id
-        self._cr.execute(
-            f"""
-            SELECT bank_id
-            FROM bank_company_line
-            WHERE res_currency_id ={get_currency.id}
-        """,
-            [list(self.ids)],
-        )
-        ids = self._cr.fetchall()
-        moves = super()._create_invoices(grouped=grouped, final=final, date=date)
-        for id in ids:
-            moves.write({"bank_company_ids": [(4, id)]})
-        return moves
+        for order in self:
+            get_currency = order.pricelist_id.currency_id
+            order._cr.execute(
+                f"""
+                SELECT bank_id
+                FROM bank_company_line
+                WHERE res_currency_id ={get_currency.id}
+            """,
+                [list(order.ids)],
+            )
+            ids = order._cr.fetchall()
+            moves = super()._create_invoices(grouped=grouped, final=final, date=date)
+            for id in ids:
+                moves.write({"bank_company_ids": [(4, id)]})
+            return moves
