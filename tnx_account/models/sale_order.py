8f5841bad0f4c7a20753df53b7c8879847814818
MaximilienNaina <maximilien@nexources.com>
2025-04-10 17:24:47 +0300

    [MERGE]: get data bank via sale order and put it in move while creation invoice from sale order

--- tnx_account/models/sale_order.py
+++ tnx_account/models/sale_order.py
@@ -10,19 +10,31 @@ class Sale_order(models.Model):
         "bank.company", "sale_order_id", string="Bank"
     )
 
+    # def _create_invoices(self, grouped=False, final=False, date=None):
+    #     for order in self:
+    #         get_currency = order.pricelist_id.currency_id
+    #         order._cr.execute(
+    #             f"""
+    #             SELECT bank_id
+    #             FROM bank_company_line
+    #             WHERE res_currency_id ={get_currency.id}
+    #         """,
+    #             [list(order.ids)],
+    #         )
+    #         ids = order._cr.fetchall()
+    #         moves = super()._create_invoices(grouped=grouped, final=final, date=date)
+    #         for id in ids:
+    #             moves.write({"bank_company_ids": [(4, id)]})
+    #         return moves
+
     def _create_invoices(self, grouped=False, final=False, date=None):
-        for order in self:
-            get_currency = order.pricelist_id.currency_id
-            order._cr.execute(
-                f"""
-                SELECT bank_id
-                FROM bank_company_line
-                WHERE res_currency_id ={get_currency.id}
-            """,
-                [list(order.ids)],
-            )
-            ids = order._cr.fetchall()
-            moves = super()._create_invoices(grouped=grouped, final=final, date=date)
-            for id in ids:
-                moves.write({"bank_company_ids": [(4, id)]})
-            return moves
+        invoices = super()._create_invoices(grouped=grouped, final=final, date=date)
+
+        sale_order_map = {order.id: order.bank_company_ids.ids for order in self}
+
+        for invoice in invoices:
+            order_id = invoice.origin_sale_id.id
+            if order_id in sale_order_map:
+                invoice.bank_company_ids = [(6, 0, sale_order_map[order_id])]
+
+        return invoices
